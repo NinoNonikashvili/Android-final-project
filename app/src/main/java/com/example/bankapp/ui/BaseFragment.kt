6918b4da351b18d4022ba964dfdfb9ef303e569b
NinoNonikashvili <nonikashvilinino8799@gmail.com>
2021-12-21 18:09:11 +0400

    add authentification design

--- app/src/main/java/com/example/bankapp/ui/BaseFragment.kt
+++ app/src/main/java/com/example/bankapp/ui/BaseFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
 import com.example.bankapp.R
 
-class BaseFragment : Fragment() {
+
+abstract class BaseFragment<T: ViewBinding>(private val inflateMethod : (LayoutInflater, ViewGroup?, Boolean) -> T) : Fragment() {
+    var _binding:T? = null
+    val binding:T
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false)
+        _binding = inflateMethod.invoke(inflater, container, false)
+        start()
+
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    abstract  fun start()
 
 }
