32bd5f4f22fdb0c53bcd4cf216a296b9914ce37c
NinoNonikashvili <nonikashvilinino8799@gmail.com>
2021-12-26 15:35:18 +0400

    add currencies and calculator fragments

--- app/src/main/java/com/example/bankapp/ui/AuthFragment.kt
+++ app/src/main/java/com/example/bankapp/ui/AuthFragment.kt
@@ -1,10 +1,17 @@
 package com.example.bankapp.ui
 
+import androidx.navigation.fragment.findNavController
+import com.example.bankapp.R
 import com.example.bankapp.databinding.FragmentAuthBinding
 
 
 class AuthFragment : BaseFragment<FragmentAuthBinding>(FragmentAuthBinding::inflate) {
     override fun start() {
+        binding.login.setOnClickListener{
+            findNavController().navigate(R.id.action_authFragment_to_currenciesFragment)
+
+        }
+
     }
 
 }
