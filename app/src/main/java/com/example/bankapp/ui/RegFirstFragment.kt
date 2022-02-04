76717c02231761181aba41498d3174cb5ab3ae95
NinoNonikashvili <nonikashvilinino8799@gmail.com>
2022-02-04 15:06:55 +0400

    changed some ui attributes

--- app/src/main/java/com/example/bankapp/ui/RegFirstFragment.kt
+++ app/src/main/java/com/example/bankapp/ui/RegFirstFragment.kt
@@ -1,20 +1,34 @@
 package com.example.bankapp.ui
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.bankapp.R
+import com.example.bankapp.RegViewModel
+import com.example.bankapp.UserInfo
 import com.example.bankapp.databinding.FragmentRegFirstBinding
 
 class RegFirstFragment : BaseFragment<FragmentRegFirstBinding>(FragmentRegFirstBinding::inflate) {
+    private val viewModel: RegViewModel by viewModels()
     override fun start() {
+        val userId = binding.userId.toString()
+        val phoneNum = binding.phoneNum.toString()
+        val firstSixNum = binding.first6numbers.toString()
+        val lastFourNum = binding.last4numbers.toString()
+        val expirationDate = binding.expireDate.toString()
+        val cvc = binding.cvcCode.toString()
+        val userInfo = UserInfo(userId, phoneNum, firstSixNum, lastFourNum, expirationDate, cvc)
+        val action = RegFirstFragmentDirections.actionRegFirstFragmentToRegSecondFragment(userInfo)
 
-        binding.next.setOnClickListener {
-            findNavController().navigate(R.id.action_regFirstFragment_to_regSecondFragment4)
+
+        binding.next.setOnClickListener{
+
         }
+
+
+
+
     }
 
 
