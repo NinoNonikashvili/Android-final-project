d1c73114c580b1c509312410fc83af98f8725715
AnanoVanidze <anano.vanidze.1@btu.edu.ge>
2022-02-14 11:59:08 +0400

    added calculator

---
+++ app/src/main/java/com/example/bankapp/currency/ConvertViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.bankapp.currency
+
+import android.util.Log.d
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bankapp.util.ApiState
+import com.example.bankapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ConvertViewModel @Inject constructor(private val currencyRepository: CurrencyRepository) :
+    ViewModel() {
+
+    private val _convertStateFlow: MutableStateFlow<ApiState> = MutableStateFlow(ApiState.Empty)
+    val convertStateFlow: StateFlow<ApiState> = _convertStateFlow
+
+
+    fun convert(
+        amount: String,
+        fromCurrency: String,
+        toCurrency: String
+    ) {
+        viewModelScope.launch {
+            _convertStateFlow.value = ApiState.Loading
+            val resultRates =
+                currencyRepository.getConvertInfo(amount, fromCurrency, toCurrency)
+
+            when (resultRates) {
+                is Resource.Error -> {
+                    _convertStateFlow.value =
+                        ApiState.Failure(resultRates.message!!)
+                }
+                is Resource.Success -> {
+                    _convertStateFlow.value = ApiState.Success(resultRates.data)
+                }
+            }
+        }
+
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/bankapp/model/ConvertInfo.kt
@@ -0,0 +1,8 @@
+package com.example.bankapp.model
+
+data class ConvertInfo(
+    val amount: Double,
+    val from: String,
+    val to: String,
+    val value: Double
+)
