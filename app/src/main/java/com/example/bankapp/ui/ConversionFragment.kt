a360348c66979a54f99f8d5f3557ad34964c6619
AnanoVanidze <anano.vanidze.1@btu.edu.ge>
2022-02-14 17:01:34 +0400

    added convert fragment details

--- app/src/main/java/com/example/bankapp/ui/ConversionFragment.kt
+++ app/src/main/java/com/example/bankapp/ui/ConversionFragment.kt
@@ -1,17 +1,79 @@
 package com.example.bankapp.ui
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.bankapp.R
+import com.example.bankapp.currency.ConvertViewModel
 import com.example.bankapp.databinding.FragmentConversionBinding
+import com.example.bankapp.extensions.invisible
+import com.example.bankapp.extensions.roundDecimal
+import com.example.bankapp.extensions.visible
+import com.example.bankapp.model.ConvertInfo
+import com.example.bankapp.util.ApiState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class ConversionFragment : BaseFragment<FragmentConversionBinding>(FragmentConversionBinding::inflate) {
 
+    private val convertViewModel: ConvertViewModel by viewModels()
+
     override fun start() {
+        setAdapter()
+        binding.convertButton.setOnClickListener {
+            convertData()
+        }
+        observe()
+
+    }
+
+    private fun setAdapter(){
+        val currencyList = ArrayAdapter(requireContext(), R.layout.list_currencies, resources.getStringArray(R.array.currency_codes))
+        binding.ATVCurrenciesDropDown.apply {
+            setText("USD")
+            setAdapter(currencyList)
+        }
+
+    }
+
+    private fun convertData() {
+        val fromCurrency = "GEL"
+        val toCurrency = binding.ATVCurrenciesDropDown.text.toString()
+        val amount = binding.forSaleMoney.text.toString()
+        convertViewModel.convert(amount, fromCurrency, toCurrency)
+
+    }
+
+    private fun observe() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                convertViewModel.convertStateFlow.collect {
+                    when (it) {
+                        is ApiState.Loading -> {
+                            binding.progressBar.visible()
+                        }
+                        is ApiState.Failure -> {
+                            Toast.makeText(requireContext(), it.msg, Toast.LENGTH_SHORT).show()
+                        }
+                        is ApiState.Success<*> -> {
+                            binding.progressBar.invisible()
+                            val result = it.data as ConvertInfo
+                            binding.convertedMoney.text = result.value.roundDecimal(2)
+                        }
+                        is ApiState.Empty -> {
+
+                        }
+                    }
+
+                }
+            }
+        }
 
     }
 
