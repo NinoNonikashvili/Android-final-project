9c4b33bb6caa4ec3cb4aa89e58e0da977603dd02
NinoNonikashvili <nonikashvilinino8799@gmail.com>
2022-02-15 22:44:29 +0400

    currency tabs are added.. some bugs need to be fixed

--- app/src/main/java/com/example/bankapp/ui/UserProfileFragment.kt
+++ app/src/main/java/com/example/bankapp/ui/UserProfileFragment.kt
@@ -1,23 +1,28 @@
 package com.example.bankapp.ui
 
 
+import android.util.Log
+import androidx.core.view.children
+import androidx.core.view.get
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.bankapp.R
 import com.example.bankapp.databinding.FragmentUserProfileBinding
+import com.example.bankapp.extensions.roundDecimal
 import com.example.bankapp.viewModels.CalculationSharedViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 
 class UserProfileFragment : BaseFragment<FragmentUserProfileBinding>(FragmentUserProfileBinding::inflate) {
-    private lateinit var auth:FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     private val addViewModel: CalculationSharedViewModel by viewModels()
     val db = Firebase.firestore
 
@@ -33,6 +38,7 @@ class UserProfileFragment : BaseFragment<FragmentUserProfileBinding>(FragmentUse
 
 
     }
+
     private fun navClicks() {
         //navigate to conversion fragment
 
@@ -57,40 +63,44 @@ class UserProfileFragment : BaseFragment<FragmentUserProfileBinding>(FragmentUse
             }
         }
     }
-    private fun logout(){
-        binding.appBar.setOnMenuItemClickListener {
-                it->
-            when(it.itemId){
-                R.id.settings-> {
+
+    private fun logout() {
+        binding.appBar.setOnMenuItemClickListener { it ->
+            when (it.itemId) {
+                R.id.settings -> {
                     auth.signOut()
                     findNavController().navigate(
-                        UserProfileFragmentDirections.actionUserProfileFragmentToAuthFragment(""))
+                        UserProfileFragmentDirections.actionUserProfileFragmentToAuthFragment("")
+                    )
                     true
-                }else-> false
+                }
+                else -> false
 
             }
         }
     }
 
-    private fun transferMoneyToYourAccount(){
+    private fun transferMoneyToYourAccount() {
         binding.enroll.setOnClickListener {
             val amount = binding.addMoney.text.toString()
 
-            if(amount.isNotEmpty()){
-                addViewModel.enroll(amount.toDouble())
+            if (amount.isNotEmpty()) {
+                addViewModel.addMoney(amount.toDouble())
                 updateAmountUI()
             }
 
         }
     }
-    private fun setAmountUI(){
-        db.collection(auth.currentUser?.uid.toString()).document("Total").get().addOnSuccessListener {
-            document->
-             document["total"]?.let{
+
+    private fun setAmountUI() {
+        db.collection(auth.currentUser?.uid.toString()).document("Total").get()
+            .addOnSuccessListener { document ->
+                document["total"]?.let {
                     binding.amount.text = it.toString()
+                }
             }
-        }
     }
+
     private fun updateAmountUI() {
         lifecycleScope.launchWhenCreated {
             addViewModel.total.collect {
@@ -98,10 +108,26 @@ class UserProfileFragment : BaseFragment<FragmentUserProfileBinding>(FragmentUse
             }
         }
     }
-    private fun addTabOnUserPage(){
-        binding.currenciesTabLayout.addTab(
-            binding.currenciesTabLayout.newTab().setText("newTab")
-        )
-    }
 
+    private fun addTabOnUserPage() {
+        val tabList = mutableMapOf<String, Int>()
+        var count = 0
+        addViewModel.updateTabs()
+        lifecycleScope.launch {
+            addViewModel.tab.collect {
+                it.forEach { (currencyName, currencyAmount) ->
+                    if(tabList.keys.contains(currencyName)){
+                        binding.currenciesTabLayout.getTabAt(tabList[currencyName]!!)
+                            ?.text = "${currencyAmount.toString().toDouble().roundDecimal(2)} $currencyName"
+                    }else{
+                        tabList[currencyName] = count
+                        binding.currenciesTabLayout.addTab(binding.currenciesTabLayout.newTab()
+                            .setText("${currencyAmount.toString().toDouble().roundDecimal(2)} $currencyName"), count)
+                        count++
+                    }
+                }
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/bankapp/viewModels/CalculationSharedViewModel.kt
+++ app/src/main/java/com/example/bankapp/viewModels/CalculationSharedViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bankapp.currency.CurrencyRepository
+import com.example.bankapp.model.NewCurrencyTab
 import com.example.bankapp.model.Total
 import com.example.bankapp.util.ApiState
 import com.example.bankapp.util.Resource
@@ -24,13 +25,14 @@ class CalculationSharedViewModel @Inject constructor(
     ViewModel() {
     val db = Firebase.firestore
     private val auth = FirebaseAuth.getInstance()
-    private val currencyList = mutableMapOf("GEL" to 0.0)
+    private val currencyList = mutableMapOf<String, Any>()
     private val userId = auth.currentUser?.uid.toString()
     private var totalAmount = Total(0.0)
     var total = MutableStateFlow(0.0)
     private val _convertStateFlow: MutableStateFlow<ApiState> = MutableStateFlow(ApiState.Empty)
     val convertStateFlow: StateFlow<ApiState> = _convertStateFlow
-
+    private val _tab = MutableStateFlow(mutableMapOf<String, Any>())
+    val tab:StateFlow<MutableMap<String, Any>> = _tab
 
 
     fun convert(
@@ -38,6 +40,12 @@ class CalculationSharedViewModel @Inject constructor(
         fromCurrency: String,
         toCurrency: String
     ) {
+        //check if amount is empty string
+        if(amount.isNullOrEmpty()){
+            _convertStateFlow.value = ApiState.Failure("enter amount")
+            return
+        }
+
         //check if the user wants to convert more than he has
         var currentGelAmount = 0.0
         db.collection(auth.currentUser?.uid.toString()).document("CurrencyList")
@@ -64,6 +72,10 @@ class CalculationSharedViewModel @Inject constructor(
                         _convertStateFlow.value = ApiState.Success(resultRates.data)
                         updateCurrencyList(resultRates.data!!.value, toCurrency)
                         updateCurrencyList(-resultRates.data!!.amount, "GEL")
+                        _tab.value = currencyList
+                        Log.d("TAG2", "changed in convert function")
+
+
 
                     }
                 }
@@ -72,7 +84,7 @@ class CalculationSharedViewModel @Inject constructor(
         }
     }
 
-    fun enroll(amount:Double){
+    fun addMoney(amount:Double){
         db.collection(userId).document("Total").get().addOnSuccessListener { document ->
 
             totalAmount.Total = document.data?.get("total")?.toString()?.toDouble()?:0.0
@@ -80,29 +92,41 @@ class CalculationSharedViewModel @Inject constructor(
             total.value = totalAmount.Total
             updateTotal()
             updateCurrencyList(amount, "GEL")
+
         }
 
     }
 
     private fun updateTotal(){
         db.collection(userId).document("Total").set( totalAmount)
-
     }
     private fun updateCurrencyList( enrolledMoney:Double, toCurrency:String){
         var oldValue = 0.0
         db.collection(userId).document("CurrencyList")
             .get().addOnSuccessListener {
-                if (it.data != null && it.data?.containsKey(toCurrency) == true)
-
+                if (it.data != null && it.data?.containsKey(toCurrency) == true) {
                     oldValue = it.data?.get(toCurrency).toString().toDouble()
+                }
 
                 val newValue = oldValue + enrolledMoney
-                Log.d("TAG2", newValue.toString())
 
                 currencyList[toCurrency] = newValue
+
                 db.collection(auth.currentUser?.uid.toString()).document("CurrencyList")
                     .set(currencyList, SetOptions.merge())
             }
+
+    }
+     fun updateTabs(){
+         Log.d("TAG2", "entered here")
+
+         db.collection(userId).document("CurrencyList").get().addOnSuccessListener {
+            it.data?.let { it ->
+                _tab.value = it
+                Log.d("TAG2", "list changed $it")
+            }
+        }
+
     }
 
 }
